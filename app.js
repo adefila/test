10dbd823128a57d2c0092d1aaddbb958d4e111fa
adefila <adefilasamuel929@gmail.com>
2020-12-15 17:47:01 +0100

    commit

--- app.js
+++ app.js
@@ -54,3 +54,17 @@ $(document).ready(function () {
 
 });
 
+// form
+document.querySelector("form").addEventListener("submit", handleSubmit);
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+    let myForm = document.getElementById('pizzaOrder');
+    let formData = new FormData(myForm)
+    fetch('/', {
+        method: 'POST',
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString()
+    }).then(() => console.log('Form successfully submitted')).catch((error) =>
+        alert(error))
+}
